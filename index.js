5831f78021b8c035ff126bfd03cb4edd492317b7
rv-git2912 <rvvish112@gmail.com>
2025-03-25 14:54:13 +0530

    hariupdatee

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    const number = +916201785597;
+    const number = +917070505903;
 
      var anchor = document.createElement('a');
      anchor.href = `https://wa.me/${number}?text=Hello%2C%20how%20are%20you%3F`;
